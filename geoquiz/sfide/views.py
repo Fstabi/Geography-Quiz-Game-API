fbdda700086bde7281edd90889d6e7205c954589
Francesco Stabilini <francesco.stabilini1@gmail.com>
2023-07-18 16:52:52 +0200

    creation Levels app

--- geoquiz/sfide/views.py
+++ geoquiz/sfide/views.py
@@ -1,20 +1,20 @@
 """
 Views for the Sfide APIs
 """
-from drf_spectacular.utils import (
-    extend_schema_view,
-    extend_schema,
-    OpenApiParameter,
-    OpenApiTypes,
-)
+# from drf_spectacular.utils import (
+#   extend_schema_view,
+#  extend_schema,
+# OpenApiParameter,
+# OpenApiTypes,
+# )
 
 from rest_framework import (
     viewsets,
-    mixins,
-    status,
+    # mixins,
+    # status,
 )
-from rest_framework.decorators import action
-from rest_framework.response import Response
+# from rest_framework.decorators import action
+# from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
