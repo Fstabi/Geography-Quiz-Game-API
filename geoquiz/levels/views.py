1374d3363beb38653c1aea5cc7a86313c4588ac9
Francesco Stabilini <francesco.stabilini1@gmail.com>
2023-08-04 14:59:28 +0200

    is_staff flag

--- geoquiz/levels/views.py
+++ geoquiz/levels/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
 from core.models import Level
+from rest_framework.authentication import TokenAuthentication
 from .serializers import LevelSerializer
 from rest_framework.permissions import IsAdminUser
 
@@ -7,4 +8,5 @@ from rest_framework.permissions import IsAdminUser
 class LevelViewSet(viewsets.ModelViewSet):
     queryset = Level.objects.all()
     serializer_class = LevelSerializer
+    authentication_classes = [TokenAuthentication]
     permission_classes = [IsAdminUser]
